1a7e1f4eea7c6f84934626e84e2182f6f7ea9ff9
Nikita Kolmogorov <backmeupplz@gmail.com>
2024-02-25 13:53:46 -0800

    Add GPIO functionality to unlock and lock the door

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,27 @@
 import 'module-alias/register'
 import 'source-map-support/register'
 
+import { Gpio } from 'onoff'
 import contract from '@/helpers/contract'
 
 void (async () => {
   console.log('Started the listener...')
   contract.on(contract.filters.Knock, async (from) => {
-    // Put the code to open the door here
-    console.log(`Got knock from ${from}`)
+    console.log(`Got knock from ${from}, unlocking the door...`)
+    try {
+      const pin = new Gpio(17, 'out')
+      pin.writeSync(1)
+      setTimeout(() => {
+        console.log('Locking the door...')
+        try {
+          pin.writeSync(0)
+        } catch (e) {
+          console.log('Unable to connect with GPIO, cannot lock the door...')
+        }
+      }, 5000)
+    } catch (e) {
+      console.log('Unable to connect with GPIO, cannot unlock the door...')
+    }
   })
   console.log('Listener started')
 })()
